693b11cfeeaf29d23f78e5c6a7c2fa0c8e750fcc
sagar8078 <sagarkumar.8078@gmail.com>
2025-12-02 23:55:53 +0530

    my first app

---
+++ firstapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test.js';
+import {Test2} from './Test.js';
+
+function App() {
+  return (
+    <div id='abc'>
+      Sagar Prajapati
+
+      <Test />
+      <Test2/>
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ firstapp/src/Test.js
@@ -0,0 +1,13 @@
+export default function Test(){
+
+    <div>
+        my Test file
+    </div>
+}
+
+export function Test2(){
+
+    <div>
+        My Test2 file
+    </div>
+}
