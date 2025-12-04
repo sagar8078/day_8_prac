f98f18ec1e99cdf42b37825464f2fc7255ffdf1c
sagar8078 <sagarkumar.8078@gmail.com>
2025-12-04 23:21:05 +0530

    secong commit

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,12 +4,21 @@ import Test from './Test.js';
 import {Test2} from './Test.js';
 
 function App() {
+
+
+  var mobile = "iphone";
+  var version= 238;
+
+  function add(){
+    console.log("Sagar Prajapati");
+  }
+
   return (
     <div id='abc'>
       Sagar Prajapati
 
-      <Test />
-      <Test2/>
+      <Test mobile = {mobile} version1={version}/>
+      <Test2 mobile = {mobile} version1={version} add1 = {add}/>
     </div>
 
     
--- firstapp/src/Test.js
+++ firstapp/src/Test.js
@@ -1,13 +1,22 @@
-export default function Test(){
+export default function Test(props){
 
+
+    var a = 321;
+    var b = "Accenture"
+    return(
     <div>
-        my Test file
+        
+        my Test file {a} and {b} and mobile is {props.mobile} and version is {props.version1} 
     </div>
+    )
 }
 
-export function Test2(){
+export function Test2({mobile, version1, add1}){
 
+    return(
     <div>
-        My Test2 file
+        My Test2 file {mobile} and {version1}
+        <button onClick = {add1}>click me</button>
     </div>
+    )
 }
